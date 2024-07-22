c8fa2efe4c5d474d2d8824e80170ef2b994af09b
Hownameeee <148776503+Hownameee@users.noreply.github.com>
2024-07-22 22:10:22 +0700

    Update client.py

--- ex1/cli/client.py
+++ ex1/cli/client.py
@@ -54,8 +54,6 @@ while True:
                     chunk = client.recv(1024)
                     if chunk == b"<EndOfFile>":
                         break
-                    if len(chunk) != 1024:
-                        count += 1
                     while len(chunk) != 1024:
                         data = client.recv(1024 - len(chunk))
                         chunk = chunk + data
@@ -66,63 +64,4 @@ while True:
     print("Reached end of input.txt. Please wait 2 seconds to read it again\n")
     sleep(2)  # Sleep before starting over
 
-# client.close()
-
-
-
-
-
-
-
-
-
-
-
-# from cgi import print_form
-# import socket
-# import signal
-# import sys
-# import os
-# from time import sleep
-
-# # Signal handler to exit program
-# def signal_handler(sig, frame):
-#     print('\nExiting program...')
-#     sys.exit(0)
-
-# def print_progress(current, total, message):
-#     progress = float(current) / float(total) * 100
-#     print(f"\rDownloading File{message}.zip... {progress:.2f}%", end='')
-
-# signal.signal(signal.SIGINT, signal_handler)
-
-# host = "127.0.0.1"
-# port = 63353
-# encoding = "utf-8"
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect((host, port))
-
-# print("Connected to", host, port)
-
-# message = client.recv(1024).decode(encoding)
-# print(message)
-
-# with open("input.txt", "r") as file:
-#     lines = file.readlines()
-#     for line in lines:
-#         line = line.strip()
-#         data = line.split(" ")
-#         if os.path.exists(data[0]) == True:
-#             continue
-#         client.sendall(line.encode(encoding))
-#         client.recv(3)
-#     client.sendall(b"ACK")
-#     fileName =[]
-#     pri =[]
-#     for i in lines:
-#         i = i.split(" ")
-#         fileName.append(i[0])
-#         pri.append(i[1])
-    
 # client.close()
